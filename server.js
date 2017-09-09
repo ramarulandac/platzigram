84ffa132b00ff80ebd15cb735450e66e64b5d59c
René Mar <rene.marulanda@gmail.com>
2017-09-09 21:50:35 +0200

    passport authentication

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 var express = require('express')
 var multer  = require('multer')
 var ext     = require('file-extension')
+
+//  Middlewares needed to work with passport
+
+var cookieParser = require('cookie-parser')
+var bodyParser = require('body-parser')
+var expressSession = require('express-session')
+var passport = require('passport')
+var flash = require('connect-flash');
+
+//  -----------------------------------------
+
+var platzigram = require('platzigram-client')
+var auth = require('./auth')
+
+//  -----------------------------------------
+
 var aws     = require('aws-sdk')
 var multerS3 = require('multer-s3')
 var config  = require('./config')
 
+//  -----------------------------------------
 
+var client = platzigram.createClient(config.client)
+const PORT = process.env.PORT || 5050
+
+//  S3 credentials
 var s3 = new aws.S3 ({
 	accessKeyId: config.aws.accessKey,
 	secretAccessKey: config.aws.secretKey
@@ -16,135 +37,177 @@ var storage = multerS3 ({
 	bucket: 'storageplatzi',
 	acl: 'public-read',
 	metadata: function (req, file, cb) {
-       cb (null, { fieldName: file.fieldname })		
+       cb (null, { fieldName: file.fieldname })
 	},
 	key: function (req, file, cb) {
 		cb (null, + Date.now() + '.' + ext (file.originalname))
 	}
 })
 
-var upload = multer({ storage: storage }).single('picture');
+var upload = multer({ storage: storage }).single('picture')
 
-var app = express()
+var app = express();
+
+//  ---------------------------------------------
+ //  bodyParser.json enables express to parse json http request
+ //  to obtain the json object in the request body
+app.set(bodyParser.json())
+//  bodyParser.urlencoded enables express to obtain form params
+app.use(bodyParser.urlencoded({ extended: false }))
+//  cookieParser enables express to response by coockie parser
+app.use(cookieParser())
+// expressSession enables session middleware
+app.use(expressSession({
+	secret: config.secret,
+	resave: false,
+	saveUnitialized: false
+}))
+//  passport initialization
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+//  ------------------------------------------------
 
 app.set('view engine', 'pug')
 
 app.use(express.static('public'))
 
-app.get('/',function(req, res){
+//  Telling express Strategy autentication and seria and deseria methods
+passport.use(auth.localStrategy)
+passport.use(auth.facebookStrategy)
+passport.deserializeUser(auth.deserializeUser)
+passport.serializeUser(auth.serializeUser)
+
+//  ---------------------------------------------------
+
+app.get('/',function (req, res) {
   res.render('index', {title:'Platzigram'})
 })
 
-app.get('/signup',function(req, res){
+app.get('/signup',function (req, res) {
   res.render('index', {title:'Platzigram - Signup'})
 })
 
-app.get('/signin',function(req, res){
+app.post('/signup', function (req, res) {
+  var user = req.body  // thanks body-parser!
+	client.saveUser(user, function (err, user) {
+		if (err) return res.status(500).send(err.message)
+
+		res.redirect('/signin')
+	})
+})
+
+//  visual link on screen
+app.get('/signin',function (req, res) {
   res.render('index', {title:'Platzigram - Signin'})
 })
 
-app.get('/api/pictures', function(req, res){
+//  real route to login
+app.use(flash())
 
-	var pictures = [
-					{	user: {
-							username: 'marulanda',
-							avatar: 'https://static.platzi.com/media/avatars/renemar_6cf3c981-36e4-40b1-9cf5-71ca5adf95bd.png'
-						 }, 
-						  url: 'office.jpg',
-						  likes: 0,
-						  liked: false,
-						  createdAt: new Date().getTime()
-					},
-					{	user: {
-							username: 'marulanda',
-							avatar: 'https://static.platzi.com/media/avatars/renemar_6cf3c981-36e4-40b1-9cf5-71ca5adf95bd.png'
-						 }, 
-						  url: 'office.jpg',
-						  likes: 1,
-						  liked: false,
-						  createdAt: new Date().setDate(new Date().getDate()-10)
-					}
-				   ]
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/signin',
+	failureFlash: true
+}))
 
-   
-     	 setTimeout(function delayLoading() {
-     	 	res.send(pictures)
-     	 }, 1500)
-    			   
+app.get('/logout', function (req, res) {
+	req.logout()
+	res.redirect('/')
 })
 
+//  Facebook Authentication
+//  Form route for facebook auth
+app.get('/auth/facebook', passport.authenticate('facebook', { scope:'email' }))
+//  route to receive authentication info
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+	successRedirect: '/',
+	failureRedirect: '/signin'
+}))
 
-app.post('/api/pictures',function(req, res) {
-	upload(req ,res , function(err){
-		if(err){
-			return res.send(500, "Error uploading file")
+//  Ensure Authentication middleware
+function ensureAuth (req, res, next) {
+	if (req.isAuthenticated()) {
+		return next()
+	}
+
+	res.status(401).send({error: 'No authenticated'})
+}
+
+app.get('/whoami', function (req, res) {
+	if (req.isAuthenticated()) {
+		return res.json(req.user)
+	}
+
+	return res.json({ auth: false })
+})
+
+app.get('/api/pictures', function (req, res) {
+
+	client.listPictures(function (err, pictures) {
+		if (err) {
+      console.log(err)
+			return res.send([])}
+
+		res.send(pictures)
+	})
+})
+
+//  route protected by passport
+app.post('/api/pictures', ensureAuth, function (req, res) {
+	upload(req, res, function (err) {
+		if (err) {
+			return res.status(500).send( `Error uploading file: ${err.message}`)
 		}
-		res.send('File uploaded successfully')
+
+		var user = req.user
+		var token = req.user.token
+	  var username = req.user.username
+		var src = req.file.location
+
+		client.savePicture({
+			src: src,
+			userId: username,
+			user: {
+				username: username,
+				avatar: user.avatar,
+				name: user.name
+			}
+		}, token, function (err, img) {
+			if (err) return res.status(500).send(err.message)
+
+			res.send(`File uploaded successfully: ${req.file.operation}`)
+		})
 	})
 })
 
 // para obtener los datos de cierto usuario!
-app.get('/api/user/:username', function(req, res) {
-	const user = {
-		username: 'marulanda',
-		avatar: 'https://static.platzi.com/media/avatars/renemar_6cf3c981-36e4-40b1-9cf5-71ca5adf95bd.png',
-		pictures: [
-			{
-				id: 1,
-				src: 'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/19367292_1791144211215780_3262724399496691712_n.jpg',
-				likes: 5
-			},
-			{
-				id: 2,
-				src: 'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/19122467_108485429765915_7999280866863874048_n.jpg',
-				likes: 3
-			},
-			{
-				id: 3,
-				src: 'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/19120891_1558003960938166_6362684295982612480_n.jpg',
-				likes: 2
-			},
-			{
-				id: 4,
-				src: 'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/18809513_172647449936360_2527427630509064192_n.jpg',
-				likes: 7
-			},
-			{
-				id: 5,
-				src: 'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/18809244_254589491612206_9181861147874164736_n.jpg',
-				likes: 1
-			},
-			{
-				id: 6,
-				src: 'https://scontent-mad1-1.cdninstagram.com/t51.2885-15/e35/18879148_1416332875093291_5094219903301320704_n.jpg',
-				likes: 4
-			}
-		]
-	}
+app.get('/api/user/:username', (req, res) => {
+	var username = req.params.username
+	client.getUser(username, function (err, user) {
+		if (err) return res.status(404).send({error: 'user not found'})
 
-    res.send(user)
-
+		res.send(user)
+	})
 })
 
 // de último se definen las rutas mas generales como la de username que acepta parámetros, las particulares como signin/signup, home van primero
 // ruta perfil de usuario
-app.get('/:username', function(req, res) {
+app.get('/:username', function (req, res) {
 	res.render('index', {title:`Platzigram - ${req.params.username}`})
 
 })
 
 // ruta imagen específica
-app.get('/:username/:id', function(req, res) {
+app.get('/:username/:id', function (req, res) {
 	res.render('index', {title:`Platzigram - ${req.params.username}`})
 
 })
 
 
-app.listen(3000, function(err) {
-  if(err) return console.log(`hubo un error ${err}`), process.exit(1)
+app.listen(PORT, function (err) {
+  if (err) return console.log(`hubo un error ${err}`), process.exit(1)
 
-  console.log(`platzigram escuchando en el puerto 3000`)
+  console.log(`platzigram escuchando en el puerto ${PORT}`)
 })
-
-
-   	
